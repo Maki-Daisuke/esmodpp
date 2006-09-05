59c52435b7c52a28622edeed0ba71d9ac5177b0e
Daisuke (yet another) Maki <maki.daisuke@gmail.com>
2006-09-05 08:57:18 +0000

    *Added `lineno' method on ESModPP::Parser. *Enhanced parsing.

--- ESModPP/Parser.pm
+++ ESModPP/Parser.pm
@@ -9,31 +9,36 @@ no warnings 'uninitialized';
 use Carp;
 
 
-use fields qw/_warning _buffer/;
-
-my $can = sub {
-    my ($self, $method) = @_;
-    $self->can($method) || $self->can("AUTOLOAD");
-};
+use fields qw/_buffer _lineno _warning/;
 
 sub new {
     my $class = shift;
     $class = ref $class || $class;
     my ESModPP::Parser $self = fields::new($class);
-    $self->{_buffer}  = "";
+    $self->{_buffer}  = "\n";
+    $self->{_lineno}  = 0;
     $self->{_warning} = 1;
     $self;
 }
 
-sub warning {
+sub warning : method {
     my ESModPP::Parser $self = shift;
     $self->{_warning} = shift  if @_;
     $self->{_warning};
 }
 
+sub lineno : method {
+    my ESModPP::Parser $self = shift;
+    $self->{_lineno};
+}
+
+my $can = sub : method {
+    my ($self, $method) = @_;
+    $self->can($method) || $self->can("AUTOLOAD");
+};
 
 # This is called when text sequence which is not a preprocessor-directive is found.
-sub directive {
+sub directive : method {
     my ESModPP::Parser $self = shift;
     my ($name, $args, $line) = @_;
     if ( $self->$can($name) ) {
@@ -44,13 +49,13 @@ sub directive {
 }
 
 # This is called when text sequence which is not a preprocessor-directive is found.
-sub text {
+sub text : method {
     my $class = ref shift;
     croak "${class}::text is not implemented";
 }
 
 # This is called when eof method is called.
-sub result {
+sub result : method {
     my $class = ref shift;
     croak "${class}::result is not implemented";
 }
@@ -59,33 +64,45 @@ sub result {
 my $terminator    = '\x{000A}\x{000D}\x{2028}\x{2029}';
 my $line          = qr{[^$terminator]*[$terminator]};
 my $white         = '\t\x{000B}\f \x{00A0}\p{IsZs}';
-my $directive     = qr{(?:[$terminator]|\A)[$white]*(//\@[A-Za-z0-9_-]+)([^$terminator]*)};
+my $directive     = qr{(//\@[A-Za-z0-9_-]+)([^$terminator]*)};
 my $literal       = qr{([^$terminator$white'"][^$terminator$white]*)};
 my $single_quoted = qr{'([^$terminator$white']*(?:''[^$terminator$white']*)*)'};
 my $double_quoted = qr{"([^$terminator$white"]*(?:""[^$terminator$white"]*)*)"};
 my $argument      = qr{$literal|$single_quoted|$double_quoted};
 
 
-sub chunk {
+sub chunk : method {
     (my ESModPP::Parser $self, my $chunk) = @_;
     $self->{_buffer} .= $chunk;
-    while ( $self->{_buffer} =~ /$directive/o ) {
-        my ($name, $args) = (substr($1, 2), $2);
-        $self->text( substr $self->{_buffer}, 0, $-[1] );
-        substr $self->{_buffer}, 0, $+[0], "";
-        my @args = ();
-        while ( $args =~ /\G[$white]+$argument/gco ) {
-             my $value  = $1;
-            (my $single = $2) =~ s/''/'/g;
-            (my $double = $3) =~ s/""/"/g;
-            push @args, "$value$single$double";
+    my $start_line = 0;
+    while ( $self->{_buffer} =~ /[$terminator]+/gco ) {
+        $start_line = $+[0];
+        if ( $self->{_buffer} =~ /\G[$white]*$directive/gco ) {
+            my ($name, $args) = (substr($1, 2), $2);
+            my $before = substr $self->{_buffer}, 0, $-[0];
+            $self->{_buffer} = substr $self->{_buffer}, $+[0];
+            $start_line = 0;
+            $self->{_lineno}++  while $before =~ /\x0D\x0A|[$terminator]/gc;
+            $self->text($before);
+            my @args = ();
+            while ( $args =~ /\G[$white]+$argument/gco ) {
+                 my $literal = $1;
+                (my $single  = $2) =~ s/''/'/g;
+                (my $double  = $3) =~ s/""/"/g;
+                push @args, "$literal$single$double";
+            }
+            unless ( $args =~ /\G[$white]*$/gco ) {
+                carp "Warning: `directive-like' line is ignored (probably, unmatched quotation?) at ", $self->lineno, ": //$name$args"  if $self->{_warning};
+                $self->text("//$name$args");
+                next;
+            }
+            $self->directive($name, \@args, "//$name$args");
         }
-        unless ( $args =~ /\G[$white]*$/gco ) {
-            carp "Warning: `directive-like' line is ignored (probably, unmatched quotation?): //$name$args"  if $self->{_warning};
-            $self->text("//$name$args");
-            next;
-        }
-        $self->directive($name, \@args, "//$name$args");
+    }
+    if ( $start_line ) {
+        my $before = substr $self->{_buffer}, 0, $start_line-1, "";
+        $self->{_lineno}++  while $before =~ /\x0D\x0A|[$terminator]/gc;
+        $self->text($before);
     }
     return 1;
 }
@@ -95,15 +112,16 @@ sub unread : method {
     $self->{_buffer} = join("", @_) . $self->{_buffer};
 }
 
-sub eof {
+sub eof : method {
     my ESModPP::Parser $self = shift;
     $self->text($self->{_buffer});
-    $self->{_buffer} = "";
+    $self->{_buffer} = "\n";
+    $self->{_lineno} = 0;
     $self->result;
 }
 
 
-sub string {
+sub string : method {
     my ($class, $text) = @_;
     $class = ref $class || $class;
     my $self = $class->new;
@@ -111,7 +129,7 @@ sub string {
     $self->eof;
 }
 
-sub handle {
+sub handle : method {
     my ($class, $fh) = @_;
     $class = ref $class || $class;
     my $self = $class->new;
@@ -119,7 +137,7 @@ sub handle {
     $self->eof;
 }
 
-sub file {
+sub file : method {
     my ($class, $file) = @_;
     $class = ref $class || $class;
     local *FILE;
